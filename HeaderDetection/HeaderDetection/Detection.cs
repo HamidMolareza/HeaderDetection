f58e41bb023384143e71b7d16eaaecff578a6063
Hamid Molareza <kitm.hamid@gmail.com>
2021-01-20 12:01:14 +0330

    Adds Detection method and testes.

---
+++ HeaderDetection/HeaderDetection/Detection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using HeaderDetection.Models;
+
+namespace HeaderDetection
+{
+    public static class Detection
+    {
+        public static ModelStructure DetectHeader(Type type)
+        {
+            if (!IsValidType(type))
+                throw new ArgumentException($"Type is not valid. ({type.Name} - {type})");
+
+            var name = GetPropertyName(type);
+            if (!HasInnerModel(type))
+                return new ModelStructure(name, 1, 0, 0, null);
+
+            var innerStructure = GetHeaderStructures(type, 1).ToList();
+            var (numOfColumns, depth) = GetDepthAndColumns(innerStructure);
+            return new ModelStructure(name, numOfColumns, 0, depth, innerStructure);
+        }
+
+        private static IEnumerable<ModelStructure> GetHeaderStructures(Type type, int depth) =>
+            type.GetProperties().Select(property => GetHeaderStructure(property, depth));
+
+        private static ModelStructure GetHeaderStructure(PropertyInfo property, int currentDepth)
+        {
+            if (!IsValidType(property.PropertyType))
+                throw new ArgumentException($"Property type is not valid. ({property.Name} - {property.PropertyType})");
+
+            var name = GetPropertyName(property);
+            if (!HasInnerModel(property.PropertyType))
+                return new ModelStructure(name, 1, currentDepth, 0, null);
+
+            var innerStructure = GetHeaderStructures(property.PropertyType, currentDepth + 1).ToList();
+            var (numOfColumns, maximumInnerDepth) = GetDepthAndColumns(innerStructure);
+            return new ModelStructure(name, numOfColumns, currentDepth, maximumInnerDepth, innerStructure);
+        }
+
+        private static string GetPropertyName(MemberInfo property)
+        {
+            var displayAttribute = property.GetCustomAttributes(typeof(DisplayNameAttribute)).FirstOrDefault();
+            if (displayAttribute is null)
+                return property.Name;
+
+            var name = ((DisplayNameAttribute) displayAttribute).DisplayName;
+            return string.IsNullOrEmpty(name) ? property.Name : name;
+        }
+
+        private static (int numOfColumns, int depth) GetDepthAndColumns(IEnumerable<ModelStructure> headers)
+        {
+            var maximumInnerDepth = 0;
+            var sum = 0;
+            foreach (var header in headers)
+            {
+                int innerDepth;
+                int numOfColumns;
+                if (header.InnerProperties is null)
+                {
+                    numOfColumns = 1;
+                    innerDepth = 0;
+                }
+                else
+                {
+                    var (columns, depth) = GetDepthAndColumns(header.InnerProperties);
+                    numOfColumns = columns;
+                    innerDepth = depth;
+                }
+
+                sum += numOfColumns;
+                innerDepth++;
+                if (innerDepth > maximumInnerDepth)
+                    maximumInnerDepth = innerDepth;
+            }
+
+            return (sum, maximumInnerDepth);
+        }
+
+        private static bool IsValidType(Type type) => !type.IsArray && !IsList(type);
+
+        private static bool IsList(Type type) => type.IsGenericType && (
+            type.GetGenericTypeDefinition() == typeof(List<>)
+            || type.GetGenericTypeDefinition() == typeof(IList<>));
+
+        private static bool HasInnerModel(Type type)
+        {
+            if (type.Namespace != null && type.Namespace.StartsWith("System"))
+                return false;
+            return !type.IsArray && !type.IsEnum && !type.IsInterface && !type.IsPointer && type.IsClass;
+        }
+    }
+}
--- HeaderDetection/Test_HeaderDetection/TestDetection.cs
+++ HeaderDetection/Test_HeaderDetection/TestDetection.cs
@@ -1,13 +1,126 @@
 using System;
+using System.Collections.Generic;
+using HeaderDetection;
+using HeaderDetection.Models;
 using Xunit;
 
 namespace Test_HeaderDetection
 {
-    public class UnitTest1
+    public class TestDetection
     {
         [Fact]
-        public void Test1()
+        public void DetectHeader_InvalidInput_ThrowArgumentException()
         {
+            var obj1 = new List<string>();
+            var obj2 = new string[1];
+
+            Assert.Throws<ArgumentException>(() => Detection.DetectHeader(obj1.GetType()));
+            Assert.Throws<ArgumentException>(() => Detection.DetectHeader(obj2.GetType()));
         }
+        
+        [Fact]
+        public void DetectHeader_InvalidSimpleModel_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => Detection.DetectHeader(typeof(InvalidSimpleModel)));
+        }
+        
+        [Fact]
+        public void DetectHeader_InvalidComplexModel_ThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => Detection.DetectHeader(typeof(InvalidComplexModel)));
+        }
+
+        [Fact]
+        public void DetectHeader_SimpleModel_ReturnStructure()
+        {
+            var result = Detection.DetectHeader(typeof(SimpleModel));
+            var expected = new ModelStructure(nameof(SimpleModel), 3, 0, 1,
+                new List<ModelStructure>
+                {
+                    new(nameof(SimpleModel.Integer), 1, 1, 0, null),
+                    new(nameof(SimpleModel.Str), 1, 1, 0, null),
+                    new(nameof(SimpleModel.Decimal), 1, 1, 0, null),
+                });
+
+            Assert.True(Equals(result, expected));
+        }
+        
+        [Fact]
+        public void DetectHeader_ComplexModel_ReturnStructure()
+        {
+            var result = Detection.DetectHeader(typeof(ComplexModel));
+            var expected = new ModelStructure(nameof(ComplexModel), 8, 0, 3,
+                new List<ModelStructure>
+                {
+                    new(nameof(ComplexModel.Guid), 1, 1, 0, null),
+                    new(nameof(ComplexModel.Simple), 3, 1, 1,
+                        new List<ModelStructure>
+                        {
+                            new(nameof(SimpleModel.Integer), 1, 2, 0, null),
+                            new(nameof(SimpleModel.Str), 1, 2, 0, null),
+                            new(nameof(SimpleModel.Decimal), 1, 2, 0, null),
+                        }),
+                    new(nameof(ComplexModel.InnerClassObj), 4, 1, 2, 
+                        new List<ModelStructure>
+                        {
+                            new(nameof(ComplexModel.Guid), 1, 2, 0, null),
+                            new(nameof(ComplexModel.Simple), 3, 2, 1,
+                                new List<ModelStructure>
+                                {
+                                    new(nameof(SimpleModel.Integer), 1, 3, 0, null),
+                                    new(nameof(SimpleModel.Str), 1, 3, 0, null),
+                                    new(nameof(SimpleModel.Decimal), 1, 3, 0, null),
+                                }),
+                        }),
+                });
+
+            Assert.True(Equals(result, expected));
+        }
+    }
+
+    public class SimpleModel
+    {
+        public int Integer { get; set; }
+        public string Str { get; set; }
+        public double Decimal { get; set; }
+    }
+    
+    public class InvalidSimpleModel
+    {
+        public int Integer { get; set; }
+        public string Str { get; set; }
+        public List<double> Decimals { get; set; }
+    }
+
+    public class ComplexModel
+    {
+        public Guid Guid { get; set; }
+        public SimpleModel Simple { get; set; }
+        public InnerClass InnerClassObj { get; set; }
+
+        public class InnerClass
+        {
+            public Guid Guid { get; set; }
+            public SimpleModel Simple { get; set; }
+        }
+    }
+    
+    public class InvalidComplexModel
+    {
+        public Guid Guid { get; set; }
+        public SimpleModel Simple { get; set; }
+        public List<InnerClass> InnerClassObj { get; set; }
+
+        public class InnerClass
+        {
+            public Guid Guid { get; set; }
+            public SimpleModel Simple { get; set; }
+        }
+    }
+
+    public class RecursiveModel
+    {
+        public string Id { get; set; }
+        public RecursiveModel InnerModel { get; set; }
     }
 }
